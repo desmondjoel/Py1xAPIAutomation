07e5c16b79dbe8c097b46f0d23cb1657eb1ee9d9
desmondjoel <joeldesmond4@gmail.com>
2023-12-12 16:58:01 +0530

    111

--- src/constants/api_constants.py
+++ src/constants/api_constants.py
@@ -1,14 +1,20 @@
-base_url="https://restful-booker.herokuapp.com"
+base_url = "https://restful-booker.herokuapp.com"
+
 
 @staticmethod
 def base_url():
     return "https://restful-booker.herokuapp.com"
+
+
 @staticmethod
 def url_create_booking():
     return "https://restful-booker.herokuapp.com/booking"
+
+
 @staticmethod
 def url_create_token():
     return "https://restful-booker.herokuapp.com/auth"
 
-def url_put_booking(self,booking_id):
-    return "https://restful-booker.herokuapp.com/booking/"+ str(self.booking_id)
+
+def url_put_booking(self, booking_id):
+    return "https://restful-booker.herokuapp.com/booking/" + str(self.booking_id)
